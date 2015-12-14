1094764ec4313f6006ceba63bb9ca718ffd06bb1
Nicolas BLANC <nicolas.r.blanc@gmail.com>
2015-12-14 16:41:05 +0100

    Change interface for resize with condition

--- src/lecteur/ui/Interface.java
+++ src/lecteur/ui/Interface.java
@@ -4,7 +4,15 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import java.awt.BorderLayout;
 
-public class Interface extends JFrame {
+import java.awt.event.ComponentListener;
+import java.awt.event.ComponentEvent;
+//import java.awt.Dimension;
+
+public class Interface extends JFrame implements ComponentListener {
+
+	private static int X_MIN = 700;
+	private static int Y_MIN = 100;
+	private static int Y_SEUIL = 200;
 
 	public Interface(String aTitle){
 		super(aTitle);
@@ -12,10 +20,37 @@ public class Interface extends JFrame {
 
 		setLayout(new BorderLayout());
 
+		this.addComponentListener(this);
+
 		add(new ControlsPanel(), BorderLayout.NORTH);
 		add(new ListPanel(), BorderLayout.CENTER);
 		pack();
+
+		this.setSize(X_MIN,Y_MIN);
 	}
+
+	public void componentResized(ComponentEvent e) {
+//		System.out.println(e.getComponent().getClass().getName() + " --- Resized : " + this.getSize().getWidth()+ " / " + this.getSize().getHeight());
+		if(this.getSize().getWidth() < X_MIN || this.getSize().getHeight() < Y_MIN) {
+			setSize(X_MIN,Y_MIN);
+		}
+
+		if (this.getSize().getHeight() < Y_SEUIL) {
+			setSize(X_MIN,Y_MIN);
+		}
+  }
+
+	public void componentHidden(ComponentEvent e) {
+//		System.out.println(e.getComponent().getClass().getName() + " --- Hidden");
+	}
+
+  public void componentMoved(ComponentEvent e) {
+//    System.out.println(e.getComponent().getClass().getName() + " --- Moved");
+  }
+
+  public void componentShown(ComponentEvent e) {
+//    System.out.println(e.getComponent().getClass().getName() + " --- Shown");
+  }
 
 	public static void main(String[] aArgs){
 		SwingUtilities.invokeLater(new Runnable() {
