9f17e7d262ab408438386006efd9f40b6d1255b6
WeHaveCookie <quentin.gerry@gmail.com>
2015-12-14 15:47:37 +0100

    ecriture du ListPanel

--- src/lecteur/ui/ListPanel.java
+++ src/lecteur/ui/ListPanel.java
@@ -2,13 +2,29 @@ package lecteur.ui;
 
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 import java.awt.BorderLayout;
 
 public class ListPanel extends JPanel {
 
     public ListPanel(){
         setLayout(new BorderLayout());
-        add(new JTable(), BorderLayout.SOUTH);
-        add(new JPanel(), BorderLayout.CENTER);
+        String[] columnNames = {"Nom",
+                        "Auteur",
+                        "Duree"};
+        Object[][] data = {{"Tuvaferkwa","Fatal Bazooka","3:45"},{"Tuvaferkwamaintenant","Fatal pistolet","4:32"}};
+
+        DefaultTableModel tableModel = new DefaultTableModel(data,columnNames) {
+          @Override
+          public boolean isCellEditable(int row, int column) {
+            return false;
+          }
+        };
+
+        JTable table = new JTable();
+        table.setModel(tableModel);
+        add(table, BorderLayout.CENTER);
+        JPanel southControl = new JPanel();
+        add(new JPanel(), BorderLayout.SOUTH);
     }
 }
