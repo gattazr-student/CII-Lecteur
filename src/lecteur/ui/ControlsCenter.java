21f8888293bd55b8c0d7eb6c4c6aabf242b7bc7d
Rémi GATTAZ <remi.gattaz@gmail.com>
2015-12-14 15:47:30 +0100

    Implementation of control centers

--- src/lecteur/ui/ControlsCenter.java
+++ src/lecteur/ui/ControlsCenter.java
@@ -2,10 +2,34 @@ package lecteur.ui;
 
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import javax.swing.JToggleButton;
+import javax.swing.JSlider;
+import javax.swing.JLabel;
 
 public class ControlsCenter extends JPanel {
 
     public ControlsCenter(){
         setLayout(new BorderLayout());
+
+        /* Panel SOUTH */
+        JPanel wPanelSouth = new JPanel();
+        wPanelSouth.setLayout(new BorderLayout());
+        wPanelSouth.add(new JSlider(0,100, 100), BorderLayout.CENTER);
+        JPanel wButtons = new JPanel();
+        wButtons.add(new JToggleButton("Equalizer"));
+        wButtons.add(new JToggleButton("Hide/Show"));
+        wPanelSouth.add(wButtons, BorderLayout.EAST);
+        this.add(wPanelSouth, BorderLayout.SOUTH);
+
+        /* Panel Center */
+        JPanel wPanelCenter = new JPanel();
+        wPanelCenter.setLayout(new BorderLayout());
+        JPanel wPanelTitles = new JPanel();
+        wPanelTitles.setLayout(new BorderLayout());
+        wPanelTitles.add(new JLabel("Fatal Bazooka - Tuvaferkwa"), BorderLayout.WEST);
+        wPanelTitles.add(new JLabel("0:00"), BorderLayout.EAST);
+        wPanelCenter.add(wPanelTitles, BorderLayout.NORTH);
+        wPanelCenter.add(new JSlider(0,100, 0), BorderLayout.CENTER);
+        this.add(wPanelCenter, BorderLayout.CENTER);
     }
 }
